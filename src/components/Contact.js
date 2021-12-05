d9f6bd1030e9984b92fd88eb4e3eecf4b7ed6489
Shasha Yan <shashayan@gmail.com>
2021-12-04 22:48:48 -0600

    add contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,20 +1,28 @@
-
 import React from "react";
+import ContactForm from "./ContactForm"
 
-const Contact = () => {
-    return (
-        <div className="has-text-white has-text-centered" id="contact" style={{position: "fixed", top: "180px"}}>
-            <br/>
-            <label className="tag label is-large title">
-                <h2>Contact Us 💌 </h2>
-            </label>
-            <br/>
-            <p>
+// const Contact = () => {
+//     return (
+//         <div className="has-text-white has-text-centered" id="contact" style={{position: "fixed", top: "180px"}}>
+//             <br/>
+//             <label className="tag label is-large title">
+//                 <h2>Contact Us 💌 </h2>
+//                 <ContactForm />
+//             </label>
+//             <br/>
+//             <p>
                 
-            </p>
+//             </p>
+//         </div>
+//     )
+// }
+
+function Contact() {
+    return (
+        <div className="contact has-text-white has-text-centered" id="contact" stype={{position: "fixed", top: "180px"}}>
+            <ContactForm />
         </div>
     )
 }
 
 export default Contact;
-
---
+++ src/components/ContactForm.js
@@ -0,0 +1,46 @@
+import React, {Component} from "react"; 
+
+
+class ContactForm extends Component {
+    state = {
+        full_name: "", 
+        phone_number: "", 
+        email: "", 
+        message: ""
+    }; 
+
+    handleOnChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleOnSubmit = event => {
+        event.preventDefault(); 
+        this.props.contactForm(this.state, this.props.history); 
+    }
+
+    render() {
+        return (
+        <div className="contact-form has-text-white has-text-centered" id="contact" style={{position: "fixed", top: "180px"}}>
+            <h2>Contact Us 💌 </h2>
+            <br/>
+            <form onSubmit={this.handleOnSubmit}>
+                <label className="label has-text-white"><strong>Full Name: </strong></label>
+                <input className="name-input" type="text" name="full_name" value={this.state.full_name} onChange={this.handleOnChange}/>
+                <br/><br/>
+                <label className="label has-text-white"><strong>Phone Number: </strong></label>
+                <input className="phone-input" type="text" name="phone_number" value={this.state.phone_number} onChange={this.handleOnChange}/>
+                <label className="label has-text-white"><strong>Email: </strong></label>
+                <input className="email-input" type="text" name="email" value={this.state.email} onChange={this.handleOnChange}/>
+                <label className="label has-text-white"><strong>Message: </strong></label>
+                <input className="message-input" type="text" name="message" value={this.state.message} onChange={this.handleOnChange}/>
+                <br/>
+                <br/>
+                <button className="button is-link" type="submit" onClick={this.handleOnSubmit}>Send!</button>
+            </form>
+        </div>
+    )}
+}
+
+export default ContactForm;
