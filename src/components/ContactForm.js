c1446f3cfb2ae988ffe1d8f3b7d2f2c30fb1dbc6
Shasha Yan <shashayan@gmail.com>
2021-12-05 22:55:41 -0600

    update

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,5 @@
 import React, {Component} from "react"; 
 
-
 class ContactForm extends Component {
     state = {
         full_name: "", 
@@ -22,14 +21,14 @@ class ContactForm extends Component {
 
     render() {
         return (
-        <div className="contact-form has-text-white has-text-centered" id="contact" style={{position: "fixed", top: "180px"}}>
+        <div className="contact-form" id="contact-form" style={{position: "fixed", top: "180px"}}>
             <h2>Contact Us 💌 </h2>
             <br/>
             <form onSubmit={this.handleOnSubmit}>
                 <label className="label has-text-white"><strong>Full Name: </strong></label>
                 <input className="name-input" type="text" name="full_name" value={this.state.full_name} onChange={this.handleOnChange}/>
                 <br/><br/>
-                <label className="label has-text-white"><strong>Phone Number: </strong></label>
+                <label className="label has-text-white"><strong>Phone #: </strong></label>
                 <input className="phone-input" type="text" name="phone_number" value={this.state.phone_number} onChange={this.handleOnChange}/>
                 <br/><br/>
                 <label className="label has-text-white"><strong>Email: </strong></label>
