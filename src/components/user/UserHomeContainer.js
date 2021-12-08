18a2bfa35852c0f8f03f31c090fed4bb250666a3
Shasha Yan <shashayan@gmail.com>
2021-12-08 00:00:36 -0600

    remove duplicate user profile

--- src/components/user/UserHomeContainer.js
+++ src/components/user/UserHomeContainer.js
@@ -16,7 +16,6 @@ const UserHomeContainer = () => {
                 </Route>
                 </Switch>
             </Router>
-            <UserProfile />
             <SideBarContainer />
             <SearchResults />
         </div>
