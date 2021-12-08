c6dec5bb3dd4ae9fc2c2cfdbee83afec407269a8
Shasha Yan <shashayan@gmail.com>
2021-12-08 04:04:00 -0600

    update contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,59 @@
-import React from "react";
-import ContactForm from "./ContactForm";
+import React, { Component } from "react";
+// import ContactForm from "./ContactForm";
 
-// const Contact = () => {
-//     return (
-//         <div className="has-text-white has-text-centered" id="contact" style={{position: "fixed", top: "180px"}}>
-//             <br/>
-//             <label className="tag label is-large title">
-//                 <h2>Contact Us 💌 </h2>
+// class Contact extends Component {
+//     render() {
+//         return (
+//             <div className="contact has-text-white has-text-centered" id="contact">
 //                 <ContactForm />
-//             </label>
-//             <br/>
-//             <p>
-                
-//             </p>
-//         </div>
-//     )
+//             </div>
+//         )
+//     }
 // }
 
-function Contact() {
-    return (
-        <div className="contact has-text-white has-text-centered" id="contact" stype={{position: "fixed", top: "180px"}}>
-            <ContactForm />
+class Contact extends Component {
+    state = {
+        full_name: "", 
+        phone_number: "", 
+        email: "", 
+        message: ""
+    }; 
+
+    handleOnChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleOnSubmit = event => {
+        event.preventDefault(); 
+        this.props.contactForm(this.state); 
+    }
+
+    render() {
+        return (
+        <div className="contact-form has-text-white has-text-centered" id="contact-form" style={{position: "fixed", top: "180px"}}>
+            <h2>Contact Us 💌 </h2>
+            <br/>
+            <form onSubmit={this.handleOnSubmit}>
+                <label className="label has-text-white"><strong>Full Name: </strong></label>
+                <input className="name-input" id="name-input" type="text" name="full_name" value={this.state.full_name} onChange={this.handleOnChange}/>
+                <br/><br/>
+                <label className="label has-text-white"><strong>Phone #: </strong></label>
+                <input className="phone-input" id="phone-input" type="text" name="phone_number" value={this.state.phone_number} onChange={this.handleOnChange}/>
+                <br/><br/>
+                <label className="label has-text-white"><strong>Email: </strong></label>
+                <input className="email-input" id="email-input" type="text" name="email" value={this.state.email} onChange={this.handleOnChange}/>
+                <br/><br/>
+                <label className="label has-text-white"><strong>Message: </strong></label>
+                <textarea className="message-input" id="message-input" type="textarea" name="message" cols="50" rows="10"
+                placeholder="Enter your message..." value={this.state.message} onChange={this.handleOnChange}></textarea>
+                <br/>
+                <br/>
+                <button className="button is-link" type="submit" onClick={this.handleOnSubmit}>Send!</button>
+            </form>
         </div>
-    )
+    )}
 }
 
 export default Contact;
