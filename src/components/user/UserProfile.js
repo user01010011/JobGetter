cef3f454ca5ec0588edcbf8ae0cce8202104274e
Shasha Yan <shashayan@gmail.com>
2021-12-07 23:16:51 -0600

    add change photo button

--- src/components/user/UserProfile.js
+++ src/components/user/UserProfile.js
@@ -16,19 +16,23 @@ class Profile extends Component {
         console.log("you clicked edit profile!")
     }
 
+    handleChangePhoto = (event) => {
+        console.log("you clicked change photo!")
+    }
+
     render() {
         return (
             <div className="profile-container" id="profile-container">
                 <div>
-    
                     <img style={{width: "160px", height:"160px", borderRadius:"80px"}}
                     src="https://images.unsplash.com/photo-1492106087820-71f1a00d2b11?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1374&q=80" 
-                    />
+                    /><br/>
+                    <button className="edit-photo" id="edit-photo" onClick={this.handleChangePhoto}>Change Photo</button>
                 </div>
                 <div>
                     <h4>Welcome, Shasha!</h4>
                     <button className="edit-profile" id="edit-profile" onClick={this.handleEditProfile}>Edit Profile</button>
-                    <h5>What jobs have you applied to today?</h5>
+                    <h4>What jobs have you applied to today?</h4>
                 </div>  
             </div>
         )
