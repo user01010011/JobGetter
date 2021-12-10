19cf2a84c53286596b61196023a4d145b7f9ceed
Shasha Yan <shashayan@gmail.com>
2021-12-10 01:30:26 -0600

    add userjobs

---
+++ src/components/user/UserJobs.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+
+class UserJobs extends Component {
+    render() {
+        return (
+            <div className="user-jobs" id="user-jobs">
+                <div className="user-jobs-applied" id="user-jobs-applied"> 
+                    <h4>Jobs Applied</h4>
+                    // render/map all jobs applied by user, ordered by most recent first.
+                </div>
+                <div className="user-favorite-jobs" id="user-favorite-jobs"> 
+                    <h4>Favorite Jobs</h4>
+                    <FavoritesBar />
+                </div>
+            </div>
+        )
+    }
+}
+
+export default UserJobs
--- src/components/user/UserResume.js
+++ src/components/user/UserResume.js
@@ -11,6 +11,26 @@ class Resume extends Component {
                 <div>
                     <a href="" target="_blank">Current Resume</a>
                 </div>
+                <br/><br/>
+                <div className="profesional-links-container" id="professional-links-container">
+                    <h4>My professional links</h4>
+                    <div className="user-linkedin" id="user-linkedin">
+                        <label class="linkedin-label">Linkedin</label>
+                        <input class="linkedin-input" name="linkedin" placeholder="URL or Username" value=""></input>
+                    </div><br/>
+                    <div className="user-github" id="user-github">
+                        <label class="github-label">Github</label>
+                        <input class="github-input" name="github" placeholder="URL or Username" value=""></input>
+                    </div><br/>
+                    <div className="user-twitter" id="user-twitter">
+                        <label class="twitter-label">Twitter</label>
+                        <input class="twitter-input" name="twitter" placeholder="URL or Username" value=""></input>
+                    </div><br/>
+                    <div className="user-website" id="user-website">
+                        <label class="website-label">Website</label>
+                        <input class="website-input" name="website" placeholder="URL or Username" value=""></input>
+                    </div><br/>
+                </div>
             </div>
 
         )
