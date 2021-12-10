203abe085f7dd17e29f9a3f5fdb2471f6449dceb
Shasha Yan <shashayan@gmail.com>
2021-12-10 01:59:31 -0600

    update

--- src/components/user/UserCompanies.js
+++ src/components/user/UserCompanies.js
@@ -3,8 +3,13 @@ import React, { Component } from "react";
 class UserCompanies extends Component {
     render() {
         return (
-            <div>
-
+            <div className="user-companies-container">
+                <div className="user-companies">
+                    <h3>My Companies</h3>
+                </div>
+                <div className="user-favorite-companies">
+                    <h3>My Favorite Companies</h3>
+                </div>
             </div>
         )
     }
