1d48ac57d9b34f811a546969368cdf1600decf58
Shasha Yan <shashayan@gmail.com>
2021-12-13 09:06:22 -0600

    update user resume

--- src/components/user/UserResume.js
+++ src/components/user/UserResume.js
@@ -1,6 +1,24 @@
 import React, { Component } from "react";
 
 class Resume extends Component {
+    state = {
+        resume: "",
+        linkedin: "",
+        github: "",
+        twitter: "",
+        website: "",
+    }
+    
+    handleOnChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        });
+    };
+
+    handleUploadResume = (evebt) => {
+        console.log("you clicked upload resume!")
+    }
+
     render() {
         return (
             <div className="resume-container" id="resume-container">
@@ -15,19 +33,19 @@ class Resume extends Component {
                 <div className="profesional-links-container" id="professional-links-container">
                     <h4>My professional links</h4>
                     <div className="user-linkedin" id="user-linkedin">
-                        <label class="linkedin-label">Linkedin</label>
+                        <label class="linkedin-label">Linkedin: </label>
                         <input class="linkedin-input" name="linkedin" placeholder="URL or Username" value=""></input>
                     </div><br/>
                     <div className="user-github" id="user-github">
-                        <label class="github-label">Github</label>
+                        <label class="github-label">Github: </label>
                         <input class="github-input" name="github" placeholder="URL or Username" value=""></input>
                     </div><br/>
                     <div className="user-twitter" id="user-twitter">
-                        <label class="twitter-label">Twitter</label>
+                        <label class="twitter-label">Twitter: </label>
                         <input class="twitter-input" name="twitter" placeholder="URL or Username" value=""></input>
                     </div><br/>
                     <div className="user-website" id="user-website">
-                        <label class="website-label">Website</label>
+                        <label class="website-label">Website: </label>
                         <input class="website-input" name="website" placeholder="URL or Username" value=""></input>
                     </div><br/>
                 </div>
