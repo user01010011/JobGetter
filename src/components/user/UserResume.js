717c9024c01ab3c87bf452fc04f36200e5a418d6
Shasha Yan <shashayan@gmail.com>
2021-12-13 09:18:37 -0600

    update professional info

--- src/components/user/UserResume.js
+++ src/components/user/UserResume.js
@@ -15,9 +15,25 @@ class Resume extends Component {
         });
     };
 
-    handleUploadResume = (evebt) => {
+    handleUploadResume = (event) => {
         console.log("you clicked upload resume!")
-    }
+    };
+
+    handleChangeLinkedin = (event) => {
+        console.log("you changed Linkedin!")
+    };
+
+    handleChangeGithub = (event) => {
+        console.log("you changed Github!")
+    };
+
+    handleChangeTwitter = (event) => {
+        console.log("you changed Twitter!")
+    };
+
+    handleChangeWebsite = (event) => {
+        console.log("you changed Website!")
+    };
 
     render() {
         return (
@@ -27,26 +43,26 @@ class Resume extends Component {
                     <button className="resume-upload" id="resume-upload" onClick={this.handleUploadResume}>Choose File</button> 
                 </div>
                 <div>
-                    <a href="" target="_blank">Current Resume</a>
+                    <a href="/resume" target="_blank">Current Resume</a>
                 </div>
                 <br/><br/>
                 <div className="profesional-links-container" id="professional-links-container">
-                    <h4>My professional links</h4>
+                    <h4>My professional links</h4><br/>
                     <div className="user-linkedin" id="user-linkedin">
                         <label class="linkedin-label">Linkedin: </label>
-                        <input class="linkedin-input" name="linkedin" placeholder="URL or Username" value=""></input>
+                        <input class="linkedin-input" name="linkedin" placeholder="URL or Username" onChange={this.handleChangeLinkedin}></input>
                     </div><br/>
                     <div className="user-github" id="user-github">
                         <label class="github-label">Github: </label>
-                        <input class="github-input" name="github" placeholder="URL or Username" value=""></input>
+                        <input class="github-input" name="github" placeholder="URL or Username" onChange={this.handleChangeGithub}></input>
                     </div><br/>
                     <div className="user-twitter" id="user-twitter">
                         <label class="twitter-label">Twitter: </label>
-                        <input class="twitter-input" name="twitter" placeholder="URL or Username" value=""></input>
+                        <input class="twitter-input" name="twitter" placeholder="URL or Username" onChange={this.handleChangeTwitter}></input>
                     </div><br/>
                     <div className="user-website" id="user-website">
                         <label class="website-label">Website: </label>
-                        <input class="website-input" name="website" placeholder="URL or Username" value=""></input>
+                        <input class="website-input" name="website" placeholder="URL or Username" onChange={this.handleChangeWebsite}></input>
                     </div><br/>
                 </div>
             </div>
